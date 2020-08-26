550fb5e720e34492a6936e9f66257efd14e2149b
MakarevichAV <makarevich.a.v@yandex.ru>
2020-08-27 00:25:56 +0300

    Навлинки для диалогов

--- src/components/DialoguesPage/DialoguesPage.jsx
+++ src/components/DialoguesPage/DialoguesPage.jsx
@@ -1,30 +1,41 @@
 import React from 'react';
 import s from './DialoguesPage.module.css';
+import { NavLink } from 'react-router-dom';
 
 const DialoguesPage = (props) => {
     return (
         <div className={s.chatRoom}>
             <div className={s.sideBar}>
-                <div className={`${s.dialogue} ${s.active}`}>
-                    <div className={s.userPhoto}></div>
-                    <p className={s.userName}>Ксения</p>
-                </div>
-                <div className={s.dialogue}>
-                    <div className={s.userPhoto}></div>
-                    <p className={s.userName}>Игорь</p>
-                </div>
-                <div className={s.dialogue}>
-                    <div className={s.userPhoto}></div>
-                    <p className={s.userName}>Марина</p>
-                </div>
-                <div className={s.dialogue}>
-                    <div className={s.userPhoto}></div>
-                    <p className={s.userName}>Галина</p>
-                </div>
-                <div className={s.dialogue}>
-                    <div className={s.userPhoto}></div>
-                    <p className={s.userName}>Твикс</p>
-                </div>
+                <NavLink activeClassName={s.active} to="/dialogues/1">
+                    <div className={s.dialogue}>
+                        <div className={s.userPhoto}></div>
+                        <p className={s.userName}>Ксения</p>
+                    </div>
+                </NavLink>
+                <NavLink activeClassName={s.active} to="/dialogues/2">
+                    <div className={s.dialogue}>
+                        <div className={s.userPhoto}></div>
+                        <p className={s.userName}>Игорь</p>
+                    </div>
+                </NavLink>
+                <NavLink activeClassName={s.active} to="/dialogues/3">
+                    <div className={s.dialogue}>
+                        <div className={s.userPhoto}></div>
+                        <p className={s.userName}>Марина</p>
+                    </div>
+                </NavLink>
+                <NavLink activeClassName={s.active} to="/dialogues/4">
+                    <div className={s.dialogue}>
+                        <div className={s.userPhoto}></div>
+                        <p className={s.userName}>Галина</p>
+                    </div>
+                </NavLink>
+                <NavLink activeClassName={s.active} to="/dialogues/5">
+                    <div className={s.dialogue}>
+                        <div className={s.userPhoto}></div>
+                        <p className={s.userName}>Твикс</p>
+                    </div>
+                </NavLink>
             </div>
             <div className={s.chat}>
                 <div className={s.msgArea}>
