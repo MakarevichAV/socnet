e3958c7e867b43f39d0dc56f175f005c9e9bcc4d
MakarevichAV <makarevich.a.v@yandex.ru>
2020-08-28 13:10:06 +0300

    созданы функции кликов по кнопкам

--- src/components/DialoguesPage/Chat/SendArea/SendArea.jsx
+++ src/components/DialoguesPage/Chat/SendArea/SendArea.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import s from './SendArea.module.css';
 
 const SendArea = () => {
+
+    const txtArea = React.createRef();
+
+    const sendMsg = () => {
+        const text = txtArea.current.value;
+        alert(text);
+    }
+
     return (
         <div className={s.sendArea}>
-            <textarea className={s.txtarea} />
-            <button className={s.btn}></button>
+            <textarea className={s.txtarea} ref={txtArea} />
+            <button className={s.btn} onClick={sendMsg}></button>
         </div>
     )
 }
--- src/components/ProfilePage/Wall/Posts/PostCreator/PostCreator.jsx
+++ src/components/ProfilePage/Wall/Posts/PostCreator/PostCreator.jsx
@@ -2,15 +2,27 @@ import React from 'react';
 import s from './PostCreator.module.css';
 
 const PostCreator = () => {
+
+    const txtArea = React.createRef();
+
+    const addPost = () => {
+        const text = txtArea.current.value;
+        alert(text);
+    }
+
     return (
         <>
             <div className={s.newPost}>
                 <div class={s.photo}></div>
-                <textarea class={s.textarea} placeholder="Что у Вас нового?" />
+                <textarea class={s.textarea} placeholder="Что у Вас нового?" 
+                    ref={txtArea}/>
             </div>
-            <button className={`${s.btn} ${s.type1}`}>Опубликовать</button>
+            <button className={`${s.btn} ${s.type1}`} onClick={addPost}>
+                Опубликовать
+            </button>
         </>
     )
+
 }
 
 export default PostCreator;
