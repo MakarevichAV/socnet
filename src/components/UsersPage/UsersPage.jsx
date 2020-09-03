15d5621b7872db9f99a626ff5b4ae2d338fc20f6
MakarevichAV <makarevich.a.v@yandex.ru>
2020-09-03 18:18:54 +0300

    компонент класс для юзеров и запрос в componentDidMount

--- src/components/UsersPage/UsersPage.jsx
+++ src/components/UsersPage/UsersPage.jsx
@@ -4,54 +4,55 @@ import cn from 'classnames';
 import * as axios from 'axios';
 import userPhoto from '../../assets/images/user-photo.jpg';
 
-const UsersPage = (props) => {
+class UsersPage extends React.Component {
 
-    if (props.users.length === 0) {
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(response => {
-                props.setUsers(response.data.items)
+                this.props.setUsers(response.data.items)
             });
     }
 
-    const users = props.users.map((user) => {
-        const btnClasses = cn(
-            s.btn,
-            { [s.follow]: !user.followed },
-            { [s.unfollow]: user.followed }
-        );
-        const photoUrl = user.photos.small != null ? user.photos.small : userPhoto;
+    render() {
+        const users = this.props.users.map((user) => {
+            const btnClasses = cn(
+                s.btn,
+                { [s.follow]: !user.followed },
+                { [s.unfollow]: user.followed }
+            );
+            const photoUrl = user.photos.small != null ? user.photos.small : userPhoto;
+            return (
+                <div key={user.id} className={s.user}>
+                    <div className={s.leftSide}>
+                        <div className={s.photo}
+                            style={{ backgroundImage: `url(${photoUrl})` }}></div>
+                        <button className={btnClasses}
+                            onClick={user.followed ?
+                                () => { this.props.unfollow(user.id) } :
+                                () => { this.props.follow(user.id) }}>
+                            {user.followed ? 'UNFOLLOW' : 'FOLLOW'}
+                        </button>
+                    </div>
+                    <div className={s.rightSide}>
+                        <div className={s.names}>
+                            <p className={s.nick}>NickName</p>
+                            <p className={s.name}>{user.name}</p>
+                            <p className={s.status}>Мой статус прост, как краткий тост</p>
+                        </div>
+                        <div className={s.location}>
+                            <p className={s.country}>Israel</p>
+                            <p className={s.city}>Tel Aviv</p>
+                        </div>
+                    </div>
+                </div>
+            )
+        });
         return (
-            <div key={user.id} className={s.user}>
-                <div className={s.leftSide}>
-                    <div className={s.photo}
-                        style={{ backgroundImage: `url(${photoUrl})` }}></div>
-                    <button className={btnClasses}
-                        onClick={user.followed ?
-                            () => { props.unfollow(user.id) } :
-                            () => { props.follow(user.id) }}>
-                        {user.followed ? 'UNFOLLOW' : 'FOLLOW'}
-                    </button>
-                </div>
-                <div className={s.rightSide}>
-                    <div className={s.names}>
-                        <p className={s.nick}>NickName</p>
-                        <p className={s.name}>{user.name}</p>
-                        <p className={s.status}>Мой статус прост, как краткий тост</p>
-                    </div>
-                    <div className={s.location}>
-                        <p className={s.country}>Israel</p>
-                        <p className={s.city}>Tel Aviv</p>
-                    </div>
-                </div>
+            <div className={`${s.userPage} container`}>
+                {users}
             </div>
         )
-    });
-
-    return (
-        <div className={`${s.userPage} container`}>
-            {users}
-        </div>
-    )
+    }
 }
 
 export default UsersPage;
