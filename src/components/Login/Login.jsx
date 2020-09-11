e26d6d6319847b6663ebb9e8bd4109a123cc6936
MakarevichAV <makarevich.a.v@yandex.ru>
2020-09-11 23:03:38 +0300

    login logout

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { reduxForm, Field } from 'redux-form';
-
+import { connect } from 'react-redux';
 import s from './Login.module.css';
 import { Input } from '../common/FormsControls/FormsControls';
 import { required, maxLengthCreator } from '../utils/validators';
+import { login } from '../../redux/authReducer';
 
 const maxLength = maxLengthCreator(26);
 const LoginForm = (props) => {
     return (
         <form className={s.form} onSubmit={props.handleSubmit}>
             <Field component={Input} validate={[required, maxLength]} className={s.input}
-                name="login" placeholder="login" />
+                name="email" placeholder="Email" type="email" />
             <Field component={Input} validate={[required, maxLength]} type="password" className={s.input}
                 name="password" placeholder="password" />
             <div className={s.checkbox}>
@@ -26,9 +27,9 @@ const LoginForm = (props) => {
 
 const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm);
 
-const Login = () => {
+const Login = (props) => {
     const onSubmit = (formData) => {
-        console.log(formData);
+        props.login(formData.email, formData.password, formData.rememberMe);
     }
     return (
         <div className={`${s.loginWindow} container`}>
@@ -37,4 +38,4 @@ const Login = () => {
     )
 }
 
-export default Login;
+export default connect(null, { login })(Login);
