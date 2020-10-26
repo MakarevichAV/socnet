223968417aaddc411aff5b1dcb5ffbdc71f34b15
MakarevichAV <makarevich.a.v@yandex.ru>
2020-10-26 14:29:54 +0300

    Updates

---
+++ precache-manifest.76e6317c0db0457754ea72404c08d0a9.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c649e9424e2d7e983711d8ea382b6605",
+    "url": "/socnet/index.html"
+  },
+  {
+    "revision": "0b990f6b86aef6324512",
+    "url": "/socnet/static/css/4.cb72cd45.chunk.css"
+  },
+  {
+    "revision": "9584d4947e57824ce284",
+    "url": "/socnet/static/css/5.20c13207.chunk.css"
+  },
+  {
+    "revision": "da8ee7c415271188a588",
+    "url": "/socnet/static/css/6.b9716a14.chunk.css"
+  },
+  {
+    "revision": "332465440bc2ee5c52cc",
+    "url": "/socnet/static/css/7.ef1ef5a5.chunk.css"
+  },
+  {
+    "revision": "978724f8a02be2524605",
+    "url": "/socnet/static/css/main.db241bfd.chunk.css"
+  },
+  {
+    "revision": "15623e09b3a429d995db",
+    "url": "/socnet/static/js/0.a5cb86b3.chunk.js"
+  },
+  {
+    "revision": "3e793f1d65cd8e7a9e36",
+    "url": "/socnet/static/js/3.1082dd81.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/socnet/static/js/3.1082dd81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b990f6b86aef6324512",
+    "url": "/socnet/static/js/4.057493a3.chunk.js"
+  },
+  {
+    "revision": "9584d4947e57824ce284",
+    "url": "/socnet/static/js/5.b6944074.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/socnet/static/js/5.b6944074.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da8ee7c415271188a588",
+    "url": "/socnet/static/js/6.3f73255b.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/socnet/static/js/6.3f73255b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "332465440bc2ee5c52cc",
+    "url": "/socnet/static/js/7.9efea4b9.chunk.js"
+  },
+  {
+    "revision": "978724f8a02be2524605",
+    "url": "/socnet/static/js/main.aa40a08c.chunk.js"
+  },
+  {
+    "revision": "c28fae2fa1518021472c",
+    "url": "/socnet/static/js/runtime-main.62746cae.js"
+  },
+  {
+    "revision": "71d49d7b76d245df41eea964b53847aa",
+    "url": "/socnet/static/media/logo.71d49d7b.svg"
+  },
+  {
+    "revision": "5264b88901e204fd139382fd93d316bd",
+    "url": "/socnet/static/media/plyaj.5264b889.jpg"
+  },
+  {
+    "revision": "6672b69e96c0ab100cd1795965551660",
+    "url": "/socnet/static/media/preloader.6672b69e.svg"
+  },
+  {
+    "revision": "a9c3f11376784d193b2d0c342db742b5",
+    "url": "/socnet/static/media/user-photo.a9c3f113.jpg"
+  },
+  {
+    "revision": "fc02d7e9ce95d50821a38b996e67eba6",
+    "url": "/socnet/static/media/user-photo.fc02d7e9.jpg"
+  }
+]);
